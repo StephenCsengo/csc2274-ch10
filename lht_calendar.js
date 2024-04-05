fb573797f629adcc9831a496574050583a1c6bf8
Stephen Csengo <stephencsengo@gmail.com>
2024-04-04 22:03:17 -0400

    Fill in calendar based on date

--- lht_calendar.js
+++ lht_calendar.js
@@ -37,6 +37,8 @@ document.getElementById("calendar").innerHTML = createCalendar(thisDay);
 function createCalendar(calDate) {
   let calendarHTML = "<table id='calendar_table'>";
   calendarHTML += calCaption(calDate);
+  calendarHTML += calWeekdayRow();
+  calendarHTML += calDays(calDate);
   calendarHTML += "</table>";
   return calendarHTML;
 }
@@ -62,4 +64,53 @@ function calCaption(calDate) {
   let thisYear = calDate.getFullYear();
   //Write the caption
   return "<caption>" + monthName[thisMonth] + " " + thisYear + "</caption>";
+}
+/*Function to write a table row of weekday abbreviations*/
+function calWeekdayRow() {
+  //Array of weekday abbreviations
+  let dayName = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+  let rowHTML = "<tr>";
+  //Loop through the dayName array
+  for (let i = 0; i < dayName.length; i++) {
+    rowHTML += "<th class='calendar_weekdays'>" + dayName[i] + "</th>";
+  }
+  rowHTML += "</tr>";
+  return rowHTML;
+}
+/*Function to calculate the number of days in the month*/
+function daysInMonth(calDate) {
+  //Array of days in each month
+  let dayCount = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  //Extract the four digit year and month value
+  let thisYear = calDate.getFullYear();
+  let thisMonth = calDate.getMonth();
+  //Revise the days in Feb for leap years
+  if (thisYear % 4 === 0) {
+    if (thisYear % 100 != 0 || thisYear % 400 === 0) {
+      dayCount[1] = 29;
+    }
+  }
+  //Return the number of days for the current month
+  return dayCount[thisMonth];
+}
+/*Function to write table rows for each day of the month*/
+function calDays(calDate) {
+  //Determine the starting day of the month
+  let day = new Date(calDate.getFullYear(), calDate.getMonth(), 1);
+  let weekDay = day.getDay();
+  //Write blank cells preceding the starting day
+  let htmlCode = "<tr>";
+  for (let i = 0; i < weekDay; i++) {
+    htmlCode += "<td></td>";
+  }
+  //Write cells for each day of the month
+  let totalDays = daysInMonth(calDate);
+  for (let i = 1; i <= totalDays; i++) {
+    day.setDate(i);
+    weekDay = day.getDay();
+    if (weekDay === 0) htmlCode += "<tr>";
+    htmlCode += "<td class='calendar_dates'>" + i + "</td>";
+    if (weekDay === 6) htmlCode += "</tr>";
+  }
+  return htmlCode;
 }
